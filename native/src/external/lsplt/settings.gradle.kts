94397c5fe9acb6d6b32d1fbab703e723794221a0
wstjf <wstjf@qq.com>
2023-07-01 10:00:15 +0800

    init

---
+++ native/src/external/lsplt/settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+    plugins {
+        id("com.android.application") version "7.4.2"
+        id("com.android.library") version "7.4.2"
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "LSPlt"
+include(
+    ":lsplt",
+    ":test",
+)
